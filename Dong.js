013ba66a387b5090ec32b0f5ef633ec0c407ab49
Alex Thomas <aothomas@bu.edu>
2017-10-29 06:57:45 -0400

    get method

--- Dong.js
+++ Dong.js
@@ -1,16 +1,16 @@
-var move = ["go","move","walk","stroll","travel","progress","advance","approach","journey","proceed","come","pursue","chase"];
+var move = ["go","move","walk","stroll","travel","progress","advance","approach","journey","proceed","come","pursue","chase","the-bath"];
 
-var flee = ["flee","escape","leave","run","dodge","evade","avoid","circumvent","depart","desert", "bail", "bolt"];
+var flee = ["flee","escape","leave","run","dodge","evade","avoid","circumvent","depart","desert", "bail", "bolt","skedaddle"];
 
 var find = ["find","search","puruse","inspect","examine","investigate","inquire","rummage","scrutinize","wild-goose chase","study","ransack","look","forage","grope","scan","check","interact"];
 
-var get = ["get","acquire","bring","earn","interact"];
+var get = ["get","acquire","bring","earn","interact","wangle"];
 
-var talk =["talk","speak","lecture","declare","converse","chat","say","communicate","utter","discuss","interact"];
+var talk =["talk","speak","lecture","declare","converse","chat","say","communicate","utter","discuss","interact", "tickle-my-mother-fucking-pickle"];
 
-var use = ["use","apply","control","utilize","wield","manipulate","expend","manipulate"];
+var use = ["use","apply","control","utilize","wield","manipulate","expend","manipulate","masturbate"];
 
-var fight = ["fight", "kill", "attack", "beat", "batter", "murder", "brutalize", "abuse", "tussle", "tiff", "war", "tug", "duel", "assault", "jump", "wrestle", "oppose", "spar", "scrap", "combat"]
+var fight = ["fight", "kill", "attack", "beat", "batter", "murder", "brutalize", "abuse", "tussle", "tiff", "war", "tug", "duel", "assault", "jump", "wrestle", "oppose", "spar", "scrap","gradutate", "combat"]
 
 function inArray(word,) {
     
