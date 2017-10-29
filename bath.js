3924f31f31d8618983b27377b811a94f69f659e0
pgilbert2017 <pgilbert2017@pingry.org>
2017-10-28 22:12:03 -0400

    fixed bug in event

--- bath.js
+++ bath.js
@@ -6,13 +6,13 @@ function Event(name, locationx, locationy, nouns, story){
 	this.name = name;
 	this.locationx = locationx;
 	this.locationy = locationy;
-	this.keywords = keywords;
+	this.nouns = nouns;
 	this.story = story;
 	this.info = function(){
 		return "I am " + this.name + " and I am located at coordinates "
-		+ this.locationx + this.locationy;
+		+ this.locationx + ", " + this.locationy;
 	}
 }
 
 //the first event filled in
-var Event0 = new Event("start", 1, 1, ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.");
+var event0 = new Event("start", 1, 1, ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.");
