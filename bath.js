6b9779a7366cbf158f7a40258473e4bcb51cb2a3
pgilbert2017 <pgilbert2017@pingry.org>
2017-10-29 01:05:30 -0400

    change bath

--- bath.js
+++ bath.js
@@ -1,5 +1,7 @@
+//THE BAAAAATH
+//
 //name is the name of the event
-//locations are the x and y coordinates
+//news is an int of the place in the array, or a string telling why you can't go that way.
 //nouns is an array of strings that contains the keywords
 //story is a string saying what is happening when they get to that event
 function Event(name, n, e, w, s, nouns, story, hint){
@@ -19,4 +21,4 @@ function Event(name, n, e, w, s, nouns, story, hint){
 }
 
 //the first event filled in
-var event0 = new Event("start", null, 1, null, null, ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.", "temp");
+var event0 = new Event("start", "", 1, "", "", ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.", "temp");
