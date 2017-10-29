ed5bdb263ee1b9a9f4681579f902acc34672e1ab
Alex Thomas <aothomas@bu.edu>
2017-10-29 04:15:18 -0400

    Kill
    Me

--- Dong.js
+++ Dong.js
@@ -1,33 +1,29 @@
-var player = {
-    currentEventIndex = 0;
-    quest: "",
-    get getQuest() {        
-        "use strict";
-        if (quest === "") {
-            return "There are currently no quests to complete!";    
-        } else {
-            return quest;
-        }
-    },                      
-    
-    get speech() {   
-        return map[xCord][yCord].hint;
-    },
-    
-    
-    
-    items: {
-            
-    }
-};
+var move = ["go","move","walk","stroll","travel","progress","advance","approach","journey","proceed","come","pursue","chase"];
 
-var verbs {
-    go: {
-        
-    }
+var flee = ["flee","escape","leave","run","dodge","evade","avoid","circumvent","depart","desert", "bail", "bolt"];
+
+var find = ["find","search","puruse","inspect","examine","investigate","inquire","rummage","scrutinize","wild-goose chase","study","ransack","look","forage","grope","scan","check","interact"];
+
+var get = ["get","acquire","bring","earn","interact"];
+
+var talk =["talk","speak","lecture","declare","converse","chat","say","communicate","utter","discuss","interact"];
+
+var use = ["use","apply","control","utilize","wield","manipulate","expend","manipulate"];
+
+var fight = ["fight", "kill", "attack", "beat", "batter", "murder", "brutalize", "abuse", "tussle", "tiff", "war", "tug", "duel", "assault", "jump", "wrestle", "oppose", "spar", "scrap", "combat"]
+
+function inArray(word,) {
     
-};
+}
 
-
+/*
+move = move.sort();
+flee = flee.sort();
+find = find.sort();
+get = get.sort();
+talk = talk.sort();
+use = use.sort();
+fight = fight.sort();
+*/
 
 
