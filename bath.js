8a3a4155af274248fe20b3d21bbb277e727ee3bc
pgilbert2017 <pgilbert2017@pingry.org>
2017-10-28 22:20:11 -0400

    added hint variable to the events

--- bath.js
+++ bath.js
@@ -2,7 +2,7 @@
 //locations are the x and y coordinates
 //nouns is an array of strings that contains the keywords
 //story is a string saying what is happening when they get to that event
-function Event(name, locationx, locationy, nouns, story){
+function Event(name, locationx, locationy, nouns, story, hint){
 	this.name = name;
 	this.locationx = locationx;
 	this.locationy = locationy;
@@ -18,4 +18,4 @@ function Event(name, locationx, locationy, nouns, story){
 }
 
 //the first event filled in
-var event0 = new Event("start", 1, 1, ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.");
+var event0 = new Event("start", 1, 1, ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.", "temp hint");
