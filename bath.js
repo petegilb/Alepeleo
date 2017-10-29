a398e8eb586e296a243be25f6fada652e9136240
pgilbert2017 <pgilbert2017@pingry.org>
2017-10-28 23:32:14 -0400

    changed events

--- bath.js
+++ bath.js
@@ -2,15 +2,16 @@
 //locations are the x and y coordinates
 //nouns is an array of strings that contains the keywords
 //story is a string saying what is happening when they get to that event
-function Event(name, locationx, locationy, nouns, story, hint){
+function Event(name, n, e, w, s, nouns, story, hint){
 	this.name = name;
-	this.locationx = locationx;
-	this.locationy = locationy;
+	this.n = n;
+	this.e = e;
+	this.w = w;
+	this.s = s;
 	this.nouns = nouns;
 	this.story = story;
 	this.info = function(){
-		return "I am " + this.name + " and I am located at coordinates "
-		+ this.locationx + ", " + this.locationy;
+		return "I am at" + this.name;
 	}
 	this.repeat = function(){
 		return story;
@@ -18,4 +19,4 @@ function Event(name, locationx, locationy, nouns, story, hint){
 }
 
 //the first event filled in
-var event0 = new Event("start", 1, 1, ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.", "temp hint");
+var event0 = new Event("start", null, 1, null, null, ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.", "temp");
