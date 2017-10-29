08193537ac06026bd4990405ff5d956024e2b99e
pgilbert2017 <pgilbert2017@pingry.org>
2017-10-29 02:40:12 -0400

    changed the bath

--- bath.js
+++ bath.js
@@ -5,20 +5,17 @@
 //nouns is an array of strings that contains the keywords
 //story is a string saying what is happening when they get to that event
 function Event(name, n, e, w, s, nouns, story, hint){
-	this.name = name;
+	this.index = index;
 	this.n = n;
 	this.e = e;
 	this.w = w;
 	this.s = s;
 	this.nouns = nouns;
 	this.story = story;
-	this.info = function(){
-		return "I am at" + this.name;
-	}
 	this.repeat = function(){
 		return story;
 	}
 }
 
 //the first event filled in
-var event0 = new Event("start", "", 1, "", "", ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.", "temp");
+var event0 = new Event(0, "Nah you probably shouldn't go that way", 1, "Nah you probably shouldn't go that way", "Nah you probably shouldn't go that way", ["sun", "trees", "grass", "ground"], "You wake up in a field with tall grass all around you, naked. The sun is starting to rise. You see some trees in the direction of the sun in the distance.", "temp");
